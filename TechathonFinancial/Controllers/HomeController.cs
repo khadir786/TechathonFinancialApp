a2df0470fb0d1528d1714b58655341aaeb3715e5
Khadir-Laptop <khadir786.ka@googlemail.com>
2024-08-15 12:28:57 +0100

    initial roles logic

--- TechathonFinancial/Controllers/HomeController.cs
+++ TechathonFinancial/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using TechathonFinancial.Models;
@@ -17,7 +18,7 @@ namespace TechathonFinancial.Controllers
         {
             return View();
         }
-
+        [Authorize]
         public IActionResult Privacy()
         {
             return View();
