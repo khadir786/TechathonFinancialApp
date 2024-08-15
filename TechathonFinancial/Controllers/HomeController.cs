aee154ac6b8b47919d59b70f752ddbadd4cbc1d0
samueljwwright <samueljwwright@gmail.com>
2024-08-15 16:20:42 +0100

    module placeholder page with nav from modules page

--- TechathonFinancial/Controllers/HomeController.cs
+++ TechathonFinancial/Controllers/HomeController.cs
@@ -32,6 +32,10 @@ namespace TechathonFinancial.Controllers
         {
             return View();
         }
+        public IActionResult Module()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
